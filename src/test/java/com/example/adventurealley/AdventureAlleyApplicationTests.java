e34fc09725184447570e2945511832bc4142e8b6
albe194e <albe194e@stud.kea.dk>
2023-03-13 13:38:02 +0100

    Initial commit

---
+++ src/test/java/com/example/adventurealley/AdventureAlleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adventurealley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureAlleyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
